d6c7ed23feab4da0fb9120073a0e93b8adce2554
Tobias Pörtner <me@tobiaspoertner.com>
2021-11-18 22:06:27 +0100

    added main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:stock_viewer/splash.dart';
 
+void main() => runApp(const StockViewer());
+
+class StockViewer extends StatelessWidget {
+  const StockViewer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+}
